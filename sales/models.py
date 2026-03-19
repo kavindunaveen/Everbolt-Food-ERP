57c28ba134cab833c87f66c58089956727527374
Kavindu <it21226878@my.sliit.lk>
2026-03-19 09:47:17 +0530

    user management

--- sales/models.py
+++ sales/models.py
@@ -8,7 +8,7 @@ class Quotation(models.Model):
     quotation_number = models.CharField(max_length=50, unique=True)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     salesperson = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    creation_date = models.DateField(auto_now_add=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
     valid_until = models.DateField()
     total_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     tax_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
@@ -55,7 +55,7 @@ class Invoice(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     salesperson = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     
-    creation_date = models.DateField(auto_now_add=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
     delivery_date = models.DateField(blank=True, null=True)
     
     total_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
